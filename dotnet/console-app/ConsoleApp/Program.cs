eda8c7d0abd68e6a10ae31c4fa85740937ce3f15
bretten <brettinbox@gmail.com>
2021-04-17 15:23:51 -0700

    Added console app test application

---
+++ dotnet/console-app/ConsoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            do
+            {
+                input = Console.ReadLine();
+                Console.WriteLine($"Input was: {input}");
+            } while (!string.IsNullOrWhiteSpace(input));
+
+            Console.WriteLine("Input was empty, stopping...");
+        }
+    }
+}
