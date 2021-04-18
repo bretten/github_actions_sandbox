9f130d6987b7e8b9c2de9a864c69751fd6cf5124
bretten <brettinbox@gmail.com>
2021-04-18 15:42:56 -0700

    Added example console app that counts down secounds

---
+++ dotnet/console-app/ConsoleAppSecondsCountdown/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading;
+
+namespace ConsoleAppSecondsCountdown
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string jobName = args[0];
+            int seconds = int.Parse(args[1]);
+
+            while (seconds > -1)
+            {
+                Console.WriteLine($"Job {jobName}: {seconds}");
+                Thread.Sleep(TimeSpan.FromSeconds(1));
+                seconds--;
+            }
+            Console.WriteLine($"Job {jobName} done");
+        }
+    }
+}
