58ff0d5adc5a70a594a46d3020d9a6f74ce5c6be
bretten <brettinbox@gmail.com>
2025-01-30 04:26:22 -0800

    Build Props

--- dotnet/console-app/ConsoleAppSecondsCountdown/Program.cs
+++ dotnet/console-app/ConsoleAppSecondsCountdown/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Reflection;
 using System.Threading;
 
 namespace ConsoleAppSecondsCountdown
@@ -7,6 +9,26 @@ namespace ConsoleAppSecondsCountdown
     {
         static void Main(string[] args)
         {
+            var buildId = Environment.GetEnvironmentVariable("BUILD_ID");
+            Console.WriteLine($"Build ID: {buildId}");
+
+            var assembly = Assembly.GetExecutingAssembly();
+            var assemblyVersion = assembly.GetName().Version;
+            var assemblyFileVersion = assembly.GetCustomAttribute<AssemblyFileVersionAttribute>()?.Version;
+            var informationalVersion = assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>()
+                ?.InformationalVersion;
+
+            Console.WriteLine($"Assembly Version: {assemblyVersion}");
+            Console.WriteLine($"Assembly File Version: {assemblyFileVersion}");
+            Console.WriteLine($"Informational Version: {informationalVersion}");
+
+            var versionInfo = FileVersionInfo.GetVersionInfo(Assembly.GetEntryAssembly()!.Location);
+            var productVersion = versionInfo.ProductVersion;
+            var fileVersion = versionInfo.FileVersion;
+
+            Console.WriteLine($"ProductVersion: {productVersion}");
+            Console.WriteLine($"FileVersion: {fileVersion}");
+
             string jobName = args[0];
             int seconds = int.Parse(args[1]);
 
@@ -16,6 +38,7 @@ namespace ConsoleAppSecondsCountdown
                 Thread.Sleep(TimeSpan.FromSeconds(1));
                 seconds--;
             }
+
             Console.WriteLine($"Job {jobName} done");
         }
     }
