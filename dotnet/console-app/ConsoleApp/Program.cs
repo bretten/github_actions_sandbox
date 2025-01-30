2c7a99cea0db32e53d87ce722cde5c0c69025773
bretten <brettinbox@gmail.com>
2025-01-30 01:42:57 -0800

    Publish .NET app with commit

--- dotnet/console-app/ConsoleApp/Program.cs
+++ dotnet/console-app/ConsoleApp/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Reflection;
 
 namespace ConsoleApp
 {
@@ -6,10 +8,31 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
+            var buildId = Environment.GetEnvironmentVariable("BUILD_ID");
+            Console.WriteLine($"Build ID: {buildId}");
+
+            var assembly = Assembly.GetExecutingAssembly();
+            var assemblyVersion = assembly.GetName().Version;
+            var assemblyFileVersion = assembly.GetCustomAttribute<AssemblyFileVersionAttribute>()?.Version;
+            var informationalVersion = assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>()
+                ?.InformationalVersion;
+
+            Console.WriteLine($"Assembly Version: {assemblyVersion}");
+            Console.WriteLine($"Assembly File Version: {assemblyFileVersion}");
+            Console.WriteLine($"Informational Version: {informationalVersion}");
+
+            var versionInfo = FileVersionInfo.GetVersionInfo(Assembly.GetEntryAssembly()!.Location);
+            var productVersion = versionInfo.ProductVersion;
+            var fileVersion = versionInfo.FileVersion;
+
+            Console.WriteLine($"ProductVersion: {productVersion}");
+            Console.WriteLine($"FileVersion: {fileVersion}");
+
             for (int i = 0; i < args.Length; i++)
             {
                 Console.WriteLine($"Input was: {args[i]}");
             }
+
             Console.WriteLine("No more arguments, stopping...");
         }
     }
